02dfebf492028ca1fd67f7f46b452be0612aeded
franciscomota <1130450@isep.ipp.pt>
2018-12-24 10:37:55 +0000

    fix(TEL-2037): Add missing class

---
+++ client/StatfulClientFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Statful.Core.Client.Buffer;
+using Statful.Core.Client.Client;
+using Statful.Core.Client.Configuration;
+using Statful.Core.Client.Server;
+using Statful.Core.Client.Utils;
+
+namespace Statful.Core.Client
+{
+    public static class StatfulClientFactory
+    {
+        public static IStatfulClient CreateStatfulClient()
+        {
+            var config = new ClientConfigurationFileReader();
+            return CreateStatfulClient(config);
+        }
+
+        public static IStatfulClient CreateStatfulClient(IClientConfiguration config)
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException("config", "Configuration cannot be null!");
+            }
+
+            var telemetronGatewayFactory = new StatfulGatewayFactory();
+            var telemetronGateway = telemetronGatewayFactory.CreateStatfulGateway(config);
+            var messageBuffer = new MessageBuffer(telemetronGateway, config.Logger, config.FlushInterval);
+            var luckyCoin = new LuckyCoin();
+            var timestamp = new Timestamp();
+            return new StatfulClient(config, messageBuffer, timestamp, luckyCoin);
+        }
+    }
+}
